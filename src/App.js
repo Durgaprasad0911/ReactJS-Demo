f0d59f0fc38405bf85f2395c91cb8ea59837566e
Durgaprasad <atukuri.prasad@gmail.com>
2022-11-30 23:32:44 +0530

    This is First comment for push the React JS code to Git Repo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import React, { Component } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hi, This is the React JS Applicaition Home Page</h2>
+    </div>
+  );
+}
+
+export default App;
